3a7b8003601bb8d621fadf2a0f6cd3fe75846e03
Abdelhamid El Bouni <elbouni2003@gmail.com>
2025-02-10 16:08:45 +0100

    login & logout func

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -25,7 +25,7 @@ export const signup = async (req,res)=>{
             // generate jwt here
             generateToken(newUser._id,res);
             await newUser.save();
-            res.status(202).json({
+            res.status(200).json({
                 _id: newUser._id,
                 email: newUser.email,
                 fullName: newUser.fullName,
@@ -42,10 +42,40 @@ export const signup = async (req,res)=>{
     
 };
 
-export const login = (req,res)=>{
-    res.send("log in");
+export const login = async(req,res)=>{
+    try {
+        const {email, password} = req.body;
+    
+        const user = await User.findOne({email});
+        if(!user) {
+            return res.status(400).json({message:"email or password are incorrect"});
+        }
+    
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if(!isPasswordCorrect) {
+            return res.status(400).json({message:"email or password are incorrect"});
+        }
+    
+        generateToken(user._id, res);
+        res.status(200).json({
+            _id: user._id,
+            email: user.email,
+            fullName: user.fullName,
+            profilePic: user.profilePic
+        });
+        
+    } catch (error) {
+        console.log("error in login controller: ", error.message);
+        res.status(500).json({message:"internal error"});
+    }
 };
 
 export const logout = (req,res)=>{
-    res.send("log out ");
+    try {
+        res.cookie("jwt","",{maxAge:0});
+        res.status(200).json({message:"log out successfully"})
+    } catch (error) {
+        console.log("error in logout controller: ", error.message);
+        res.status(500).json({message:"internal error"});
+    }
 };
